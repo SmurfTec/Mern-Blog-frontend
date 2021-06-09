93ebad329b1c8b0ae4db8c5c3ed4e280148f87a0
U Ahmad <UmadAhmad1928@gmail.com>
2021-06-09 22:38:56 +0500

    Cleaned Up Code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import Link from "components/Link";
-
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link href="https://reactjs.org">Learn React</Link>
-      </header>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
